91dd83342a31bf51f68615db28f50e36cd988d6d
zuzana_fertalova <xfertalova@stuba.sk>
2022-03-28 18:38:52 +0200

    first page frontend

---
+++ app/src/main/java/com/example/gastrozone/RegisterFirmActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gastrozone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register_firm.*
+
+class RegisterFirmActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_firm)
+
+    }
+}
