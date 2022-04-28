5603647f141f82ffdbf6cf4f5f276bab9a4aba27
zuzana_fertalova <xfertalova@stuba.sk>
2022-04-28 17:45:27 +0200

    firstSettingsFIX

--- app/src/main/java/com/example/gastrozone/HomeScreenActivity.kt
+++ app/src/main/java/com/example/gastrozone/HomeScreenActivity.kt
@@ -18,21 +18,36 @@ import kotlinx.android.synthetic.main.bottom_sheet_filters.*
 import org.json.JSONObject
 import org.json.JSONTokener
 
-class HomeScreenActivity : AppCompatActivity() {
+class HomeScreenActivity : AppCompatActivity(), BottomSheetTypeOfFirm.BottomSheetListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
         val token = getIntent().getStringExtra("token")
-        val baseUrl = "http://10.0.2.2:5000"
+        val baseUrl = "http://37.9.170.36:8080"
         var type = 5
-        btnShowOptions.setOnClickListener(View.OnClickListener{
+
+//ak je to user, tak nastavenie typu podniku zmizne
+        Thread(Runnable {
+            val httpClient = HttpActivity()
+            val data = httpClient.ExecGETRequest(uri = "$baseUrl/v1/api/user")
+            val jsonObject = JSONTokener(data).nextValue() as JSONObject
+            val is_company = jsonObject.getString("is_company").toBoolean()
+            if (!is_company) {
+                tvChangeTypPodniku.visibility = View.GONE
+                btnShowOptions.visibility = View.GONE
+                tvTypPodnikuChosen.visibility = View.GONE
+            }
+        }).start()
+
+
+        /*btnShowOptions.setOnClickListener(View.OnClickListener {
             setContentView(R.layout.bottom_sheet_filters)
             tvRestauraciaFilter.setOnClickListener(View.OnClickListener {
                 type = 1
             })
-        })
-        btnSetFirstSettings.setOnClickListener(View.OnClickListener{
+        })*/
+        btnSetFirstSettings.setOnClickListener(View.OnClickListener {
             Thread(Runnable {
                 val httpClient = HttpActivity()
                 val name: String = tvSetUsername.text.toString()
@@ -44,38 +59,59 @@ class HomeScreenActivity : AppCompatActivity() {
 
         })
 
-        setViewPager()
 
-        /*fetch_data_btn.setOnClickListener {
-            Thread(Runnable {
-                val httpClient = HttpActivity()
-                val data = httpClient.ExecGETRequest(uri = "$baseUrl/v1/api/user")
+        setUsernameFragment()
+        setUpOpenBottomSheetTypeOfFirm()
+        //btnSetUsernameSetClick()
+    }
 
-                println(data)
-                val jsonObject = JSONTokener(data).nextValue() as JSONObject
-                val id = jsonObject.getString("id")
-                val name = jsonObject.getString("name")
-                val type = jsonObject.getString("type")
-                val is_company = jsonObject.getString("is_company").toBoolean()
+    fun setUsernameFragment() {
 
+        var username: String = ""
+
+        Thread(Runnable {
+            val baseUrl = "http://37.9.170.36:8080"
+            val httpClient = HttpActivity()
+            val data = httpClient.ExecGETRequest(uri = "$baseUrl/v1/api/user")
+            val jsonObject = JSONTokener(data).nextValue() as JSONObject
+            username = jsonObject.getString("name")
+            if (!username.isEmpty()) {
                 runOnUiThread {
-                    Toast.makeText(this, data, Toast.LENGTH_SHORT).show()
+                    fragmentSetUserame.view?.visibility = View.GONE
+                    setViewPager()
                 }
-            }).start()*/
+            }
+        }).start()
+    }
+
+    fun setUpOpenBottomSheetTypeOfFirm() {
+        btnShowOptions.setOnClickListener {
+            val bottomSheet = BottomSheetTypeOfFirm(this)
+            supportFragmentManager.let { it1 ->
+                bottomSheet.show(
+                    it1,
+                    "BottomSheetDialogTypPodniku"
+                )
+            }
         }
+    }
+
+    override fun onOptionClick(text: String) {
+        tvTypPodnikuChosen.text = text
+    }
 
 
-        fun setViewPager() {
-            val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
-            viewPagerAdapter.addManagerProfile(SearchFragment(), "")
-            viewPagerAdapter.addManagerProfile(HomeScreenFragment(), "")
-            viewPagerAdapter.addManagerProfile(ProfileFragment(), "")
-            viewPager.adapter = viewPagerAdapter
-            viewPager.currentItem = 1
-            tabs.setupWithViewPager(viewPager)
-            tabs.getTabAt(0)?.setIcon(R.drawable.ic_find_24dp)
-            tabs.getTabAt(1)?.setIcon(R.drawable.ic_home)
-            tabs.getTabAt(2)?.setIcon(R.drawable.ic_person_24dp)
+    fun setViewPager() {
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPagerAdapter.addManagerProfile(SearchFragment(), "")
+        viewPagerAdapter.addManagerProfile(HomeScreenFragment(), "")
+        viewPagerAdapter.addManagerProfile(ProfileFragment(), "")
+        viewPager.adapter = viewPagerAdapter
+        viewPager.currentItem = 1
+        tabs.setupWithViewPager(viewPager)
+        tabs.getTabAt(0)?.setIcon(R.drawable.ic_find_24dp)
+        tabs.getTabAt(1)?.setIcon(R.drawable.ic_home)
+        tabs.getTabAt(2)?.setIcon(R.drawable.ic_person_24dp)
 
     }
 }
--- app/src/main/java/com/example/gastrozone/LoginActivity.kt
+++ app/src/main/java/com/example/gastrozone/LoginActivity.kt
@@ -27,7 +27,7 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Vyplňte všetky polia", Toast.LENGTH_SHORT).show()
             } else {
                 Thread(Runnable {
-                    val url = "http://10.0.2.2:5000/login"
+                    val url = "http://37.9.170.36:8080/login"
                     val jsonPost = "$email:$password"
                     println(jsonPost)
                     val encodedString: String = Base64.getEncoder().encodeToString(jsonPost.toByteArray())
